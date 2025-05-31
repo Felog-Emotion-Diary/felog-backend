0ae3764e2c4b8743a930cf1eef4f493522004de7
kimeunsu <kim990824@jbnu.ac.kr>
2025-05-31 16:00:19 +0900

    feat: 비밀번호 재설정 기능 구현

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -8,4 +8,12 @@ router.post('/register', async (req: Request, res: Response) => {
   await userController.register(req, res);
 });
 
+router.post('/reset', async (req: Request, res: Response) => {
+  await userController.requestPasswordReset(req, res);
+});
+
+router.post('/reset/:token', async (req: Request, res: Response) => {
+  await userController.resetPassword(req, res);
+});
+
 export default router;
