8e015aaf1d2213ee46f327e55732e4ade9d62a8e
CreateWater <blackrp001@naver.com>
2025-05-31 09:09:04 +0900

    chore: userService 클래스 스켈레톤 생성
    Co-authored-by: andrew0416 <andrew0416@naver.com>

---
+++ src/services/user.service.ts
@@ -0,0 +1,16 @@
+import * as bcryptUtil from '../utils/bcrypt';
+
+export class UserService {
+  
+  async registerUser(email: string, password: string) {
+    
+  }
+
+  async changePassword(userId: string, newPassword: string) {
+    
+  }
+
+  async validatePassword(email: string, password: string) {
+    
+  }
+}
