36e3aa717e9b32ec3ac47ed5fe21dd969c2f95d2
kimeunsu <kim990824@jbnu.ac.kr>
2025-06-09 13:38:05 +0900

    cors 설정 추가

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import cors from "cors";
 import userRouter from './routers/user.router';
 import diaryRouter from './routers/diary.router';
 
 const app = express();
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/users', userRouter);
