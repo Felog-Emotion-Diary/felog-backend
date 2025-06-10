328e621987954194f735b986ca457d33bc3b4e5b
CreateWater <blackrp001@naver.com>
2025-06-10 15:26:08 +0900

    app.ts에 라우트 추가
    Co-authored-by: andrew0416 <andrew0416@naver.com>

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import userRouter from './routers/user.router';
 import diaryRouter from './routers/diary.router';
+import statisticsRouter from './routers/statistics.router';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -13,7 +14,8 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/users', userRouter);
-app.use('/api/diaries', diaryRouter); 
+app.use('/api/diaries', diaryRouter);
+app.use('/api/statistics', statisticsRouter); 
 
 app.set("port", process.env.PORT || 3000);
 
